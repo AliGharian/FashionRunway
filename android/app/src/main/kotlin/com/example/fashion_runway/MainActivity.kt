7ab24aa15d413584bd3898263ebf9112678d04f0
AliGharian <ali.gharian98@gmail.com>
2020-07-10 11:03:57 +0430

    initial commit

---
+++ android/app/src/main/kotlin/com/example/fashion_runway/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fashion_runway
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:fashion_runway/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
