c6e49b9a8448e038e06df80c3734119dbe89d60e
AliGharian <ali.gharian98@gmail.com>
2021-03-12 19:15:15 +0330

    add search field to navigation sidebar and add user icon, there is some error with responsive of sidebar :)

--- lib/widgets/side_nav.dart
+++ lib/widgets/side_nav.dart
@@ -1,19 +1,27 @@
+import 'package:fashion_runway/pages/products_page.dart';
 import 'package:flutter/material.dart';
 import '../constants.dart';
 
-List<String> items = ['Home', 'Products', 'Contacts', 'About'];
+List<String> items = [
+  'Home',
+  'Products',
+  'Contacts',
+  'About',
+];
 
 class SideNav extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 20.0),
       color: kDarkBlue,
       height: size.height,
       width: size.width * 1 / 9,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          SearchField(),
           TopIcons(),
           LogoType(text: 'MARC JACOBS'),
           Column(
@@ -21,17 +29,55 @@ class SideNav extends StatelessWidget {
             children: items.map((String item) {
               return Padding(
                 padding: EdgeInsets.symmetric(vertical: 16.0),
-                child: Text(
-                  item,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
+                child: InkWell(
+                  onTap: () {},
+                  child: Text(
+                    item,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
                   ),
                 ),
               );
             }).toList(),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(color: Colors.white),
+      cursorColor: Colors.white10,
+      decoration: InputDecoration(
+        focusColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(90.0)),
+          //borderSide: const BorderSide(),
+        ),
+        prefixIcon: IconButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (contex) => ProductsPage()));
+          },
+          icon: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+        ),
+        filled: true,
+        fillColor: Colors.white10,
+        hintText: 'Search...',
+        hintStyle: TextStyle(color: Colors.grey),
       ),
     );
   }
@@ -65,17 +111,26 @@ class TopIcons extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Icon(
-            Icons.search,
-            color: Colors.white,
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
           ),
-          Icon(
-            Icons.shopping_basket,
-            color: Colors.white,
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_basket,
+              color: Colors.white,
+            ),
           ),
-          Icon(
-            Icons.favorite_border,
-            color: Colors.white,
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.white,
+            ),
           ),
         ],
       ),
